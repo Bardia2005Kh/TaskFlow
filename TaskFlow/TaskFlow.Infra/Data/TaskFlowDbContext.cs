d9dd6c514c934053eaa8110c25e1d760385f74ec
Bardia2005Kh <142528657+Bardia2005Kh@users.noreply.github.com>
2025-09-01 19:45:36 +0330

    DI + DbComtext Setup + Relations
    Co-Authored-By: Bardia <142528657+bardia2005kh@users.noreply.github.com>

---
+++ TaskFlow/TaskFlow.Infra/Data/TaskFlowDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskFlow.Domain.Models;
+
+namespace TaskFlow.Infra.Data
+{
+    public class TaskFlowDbContext : DbContext
+    {
+        public TaskFlowDbContext(DbContextOptions<TaskFlowDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> users { get; set; }
+    }
+}
--- TaskFlow/TaskFlow.WebAPI/Program.cs
+++ TaskFlow/TaskFlow.WebAPI/Program.cs
@@ -1,3 +1,5 @@
+using TaskFlow.Infra.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TaskFlowDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ResturantsConnectionString")));
 
 var app = builder.Build();
 
